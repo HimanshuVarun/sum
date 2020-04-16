bcdf1bf1e98504e2339d5599142dadf4333c1410
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-16 16:46:12 +0530

    first commit

---
+++ src/test/java/Sum/Sum/Open.java
@@ -0,0 +1,31 @@
+package Sum.Sum;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Open 
+{
+	@BeforeTest
+	public void hyy()
+	{
+		System.out.println("opennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+	}
+	@Test
+	public void hhh()
+	{
+		System.out.println("hyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
+	}
+	@Test
+	public void kk()
+	{
+		int a,b,c;
+		a=2;
+		b=4;
+		c=a+b;
+		System.out.println("total addtion issssssss"+c);
+				
+	}
+	
+	
+
+}
